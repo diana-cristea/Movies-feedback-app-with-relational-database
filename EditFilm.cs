56b0b88cf9197046c11b116e10aee0911d8ad303
diana-cristea <68387336+diana-cristea@users.noreply.github.com>
2021-03-14 21:35:40 +0200

    Update EditFilm.cs

--- EditFilm.cs
+++ EditFilm.cs
@@ -21,7 +21,7 @@ namespace ProiectBD
         {
            
         }
-        //la incarcarea paginii dedicate actualizarii de date se afiseaza datele in format initial
+        // when loading the page dedicated to updating data, the data is displayed in initial format
         private void EditFilm_Load(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-MFIROOJ\SQLEXPRESS;Initial Catalog=MovieFeedbackandClassif;Integrated Security=True");
@@ -34,7 +34,7 @@ namespace ProiectBD
             dataGridView1.DataSource = dt;
             con.Close();
         }
-        //functie de afisare date din tabela Filme
+        // data display function from the Movies table
         private void DisplayData()
         {
             SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-MFIROOJ\SQLEXPRESS;Initial Catalog=MovieFeedbackandClassif;Integrated Security=True");
@@ -47,7 +47,7 @@ namespace ProiectBD
             dataGridView1.DataSource = dt;
             con.Close();
         }
-        //functie de stergere text din casete
+        // delete text function from boxes
         private void ClearData()
         {
             Titlu.Text = "";
@@ -67,7 +67,7 @@ namespace ProiectBD
         {
 
         }
-        //atunci cand se apasa in dreptul unui rand, casetele text preiau datele filmului selectat pentru a putea fi modificate
+        // when the used clicks on a row, the text boxes take over the data of the selected movie so that it can be modified
         private void dataGridView1_RowHeaderMouseClick_1(object sender, DataGridViewCellMouseEventArgs e)
         {
             ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
@@ -79,15 +79,15 @@ namespace ProiectBD
             Descriere.Text = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
             Regizor.Text = dataGridView1.Rows[e.RowIndex].Cells[7].Value.ToString();
         }
-        //functie ce se activeaza la apasarea butonului de actualizare date film
+        // function that is activated when the movie data update button is pressed
         private void button1_Click(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-MFIROOJ\SQLEXPRESS;Initial Catalog=MovieFeedbackandClassif;Integrated Security=True");
-            //comanda sql pentru actualizarea datelor in tabela Filme
+            // sql command for updating data in the Movies table
             string query = "Update Filme set Titlu=@titlu, An_aparitie=@an, Durata=@durata, Tara_film=@tara, Limba=@limba, Descriere=@descriere, Regizor_principal=@regizor Where FilmID=@id";
             con.Open();
             SqlCommand com = new SqlCommand(query, con);
-            //Se preiau parametrii modificati din casetele text
+            // Take the modified parameters from the text boxes
             com.Parameters.AddWithValue("@id", ID);
             com.Parameters.AddWithValue("@titlu",Titlu.Text);
             com.Parameters.AddWithValue("@an", An.Text);
@@ -99,8 +99,8 @@ namespace ProiectBD
             com.ExecuteNonQuery();
             MessageBox.Show("Inregistrare actualizata cu succes!"); //se afiseaza un mesaj de informare 
             con.Close();
-            DisplayData(); //se afiseaza datele actualizate
-            ClearData(); //se sterg datele din casetele text
+            DisplayData(); // the updated data is displayed
+            ClearData(); // delete the data in the text boxes
         }
     }
 }
